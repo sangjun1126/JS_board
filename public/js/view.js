cc5c48cb4cc555e5807f1790c4b3462146d1d02d
sangjun1126 <jeongsangjun1234@gmail.com>
2024-03-13 16:33:25 +0900

    CRUD DELETE 로직 구현

--- public/js/view.js
+++ public/js/view.js
@@ -7,6 +7,7 @@ const board = boardsObj[index];
 
 const beforeUrl = document.referrer;
 
+// 조회수 로직
 const viewCount = (beforeUrl) => {
     if (beforeUrl.split("/").pop() === "list.html") {
         board.views++;
@@ -17,6 +18,8 @@ const viewCount = (beforeUrl) => {
 
 viewCount(beforeUrl);
 
+
+// 데이터 출력하기
 const viewFrm = document.querySelectorAll("#viewFrm > div");
 
 for (let i = 0; i < viewFrm.length; i++) {
@@ -24,6 +27,8 @@ for (let i = 0; i < viewFrm.length; i++) {
     viewFrm[i].innerHTML += " " + board[id];
 }
 
+
+// 수정 버튼
 const modifyBtn = document.querySelector("#modify");
 
 const modifyBtnHandler = (e) => {
@@ -31,3 +36,20 @@ const modifyBtnHandler = (e) => {
 }
 
 modifyBtn.addEventListener("click", modifyBtnHandler);
+
+// 삭제 버튼
+const deleteBtn = document.querySelector("#delete");
+
+const deleteBtnHandler = (e) => {
+    boardsObj.splice(index, 1);
+    
+    for (let i = 0; i < boardsObj.length; i++) {
+        boardsObj[i].index = i;
+    }
+
+    const setBoardStr = JSON.stringify(boardsObj);
+    localStorage.setItem("boards", setBoardStr);
+    location.href = "/board/list.html";
+}
+
+deleteBtn.addEventListener("click", deleteBtnHandler);
